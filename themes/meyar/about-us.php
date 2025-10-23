361fdf5c6d04e0036f0811dbbe02c9b4adabc682
Mohammad <remap0095@yahoo.com>
2025-10-24 01:16:16 +0330

    work on about us page

--- themes/meyar/about-us.php
+++ themes/meyar/about-us.php
@@ -11,7 +11,7 @@ get_header(); ?>
             <div class="row">
                 <div class="col-md-8">
                     <div class="about-hero-right">
-                        <h1 class="mb-4 Dana-ExtraBold">معیار در یک نگاه</h1>
+                        <h3 class="mb-4 Dana-ExtraBold">معیار در یک نگاه</h3>
                         <p class="mb-4 Dana-Medium">معیـــار در یک نــگاه معیار به‌عنوان یک بانک جامع خدمات روانشناختی، مدیریت سازمانی و منابع انسانی، مجموعه‌ای متشکل از بیش از ۵۰ کارگروه تخصصی و سازمانی است که شامل مشاوران و متخصصان حوزه روانشناسی بالینی و صنعتی-سازمانی، مهندسان صنایع، اساتید دانشگاه، روان‌سنجان، کارشناسان بهداشت و ایمنی و سایر حرفه‌ای‌های مرتبط می‌باشد. </p>
                         <p class="mb-4 Dana-Medium">معیار با تکیه بر دانش فنی، علمی و سال‌ها تجربه، هدف دارد شبکه‌ای تخصصی و یکپارچه ایجاد کند تا سازمان‌ها بتوانند تمامی نیازهای خود در این حوزه‌ها را برطرف کنند و نیازی به مراجعه به منابع و سایت‌های مختلف برای دریافت خدمات متنوع نداشته باشند. </p>
                         <p class="Dana-Medium">در مجموعه معیار، ما فضایی فراهم کرده‌ایم تا شما با اطمینان خاطر بتوانید به آرمان‌ها و اهداف سازمانی خود دست یابید.</p>
@@ -34,10 +34,82 @@ get_header(); ?>
                 </div>
                 <div class="col-md-4 about-why-left">
                     <div class="about-why-left-item">
-                        <p></p> 
-                        <h3 class="Dana-ExtraBold">معیار در یک نگاه</h3>
+                        <p class="pointer"></p> 
+                        <h3 class="Dana-ExtraBold">یکپارچگی خدمات</h3>
+                    </div>
+                    <div class="about-why-left-item">
+                        <p class="pointer"></p> 
+                        <h3 class="Dana-ExtraBold">بهره‌گیری از دانش روز و تجربه‌های عملیاتی</h3>
+                    </div>
+                                        <div class="about-why-left-item">
+                        <p class="pointer"></p> 
+                        <h3 class="Dana-ExtraBold">طراحی راهکارهای سفارشی‌سازی شده</h3>
+                    </div>
+                    <div class="about-why-left-item">
+                        <p class="pointer"></p> 
+                        <h3 class="Dana-ExtraBold">جامعیت و تنوع تخصص ها</h3>
+                    </div>
+                    <div class="about-why-left-item">
+                        <p class="pointer"></p> 
+                        <h3 class="Dana-ExtraBold">پشتیبانی مستمر و حرفه ای</h3>
+                    </div>
+                    <div class="about-why-left-item">
+                        <p class="pointer"></p> 
+                        <h3 class="Dana-ExtraBold">تمرکز بر توسعه پایدار و ارزش های سازمانی</h3>
                     </div>
                 </div>  
+            </div>
+        </div>
+    </section>
+
+    <section class="about-overview">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="about-overview-right">
+                        <h3 class="Dana-ExtraBold">چشم انداز</h3>
+                        <p class="Dana-Medium">چشم‌انداز معیار، تبدیل شدن به خانه امن توسعه سازمانی برای تمام کسب‌وکارهای ایرانی است؛ جایی که هر سازمانی بتواند متناسب با اندازه و صنعت خود، مسیر رشد شفاف و علمی را طی کند.</p>
+                        <h3 class="Dana-ExtraBold">مــاموریت</h3>
+                        <p class="Dana-Medium">از ماموریت های ما افزایش انگیزه شغلی، تعهد کاری، رفتار شهروندی سازمانی، و رضایت شغلی پرسنل می باشد. همچنین افزایش انعطاف پذیری و سازگاری با تغییرات لازم برای توسعه و موفقیت سازمان، از دیگر ماموریت‌های ماست.</p>
+                        <h3 class="Dana-ExtraBold">ارزش هـــا</h3>
+                        <div class="about-why-left-item">
+                            <h5 class="pointer"></h5> 
+                            <h5 class="mb-0 Dana-Medium">خلاقیت ضابطه‌مند: ترکیب نوآوری با چارچوب‌های علمی</h5>
+                        </div>
+                        <div class="about-why-left-item">
+                            <h5 class="pointer"></h5> 
+                            <h5 class="mb-0 Dana-Medium">اثربخشی پایدار: تمرکز بر نتایج ملموس و بلندمدت </h5>
+                        </div>
+                        <div class="about-why-left-item">
+                            <h5 class="pointer"></h5> 
+                            <h5 class="mb-0 Dana-Medium">مسئولیت اجتماعی: توسعه منابع انسانی به‌عنوان موتور محرکه اقتصاد ملی</h5>
+                        </div>
+                        <div class="about-why-left-item">
+                            <h5 class="pointer"></h5> 
+                            <h5 class="mb-0 Dana-Medium">انعطاف پذیری: سازگاری و قابلیت هماهنگی با نیازهای مشتریان</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 about-overview-left">
+
+                </div>  
+            </div>
+        </div>
+    </section>
+
+   <section class="about-target">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="about-target-right">
+
+                    </div>
+                </div>
+                <div class="col-md-6 about-target-left">
+                    <h3 class="Dana-ExtraBold">اهداف</h3>
+                    <p class="Dana-Medium">ارائه خدمات مشاوره ای و اجرای پروژه در زمینه تشخیص و حل مسائل سازمان، توانمندسازی کارکنان، برگزاری دوره های آموزشی، مداخله های سازمانی جهت بهبود فرآیندها و فرهنگ کاری، ارتقاء بهره وری سازمان، افزایش سلامت روانی و جسمانی و سطح رضایت شغلی پرسنل می باشد.</p>
+                    <h3 class="Dana-ExtraBold">سوابق همکاری و مسیر فعالیت ها</h3>
+                    <p class="Dana-Medium">مجموعه معیار به عنوان یکی از اعضای حقوقی هلدینگ ایما (دومین  هلدینگ خدمات روانشناسی و آموزشی کشور)  پس از کسب سالها تجربه در این حوزه، فعالیت خود را با موضوع خدمات آموزشی، مشاوره ای و ارزشیابی واحدهای منابع انسانی و مدیریت سازمانی توسعه داده و از سال 1404 به صورت نهاد مستقل مشغول به فعالیت است. از سوابق کاری این مجموعه، همکاری با واحدهای مدیریت منابع انسانی و رفاه شرکت هایی مانند کروز ، ارتباطات الکترونیک فن آوا، شرکت نفت، شرکت داتین، بیمه گری پاسارگاد، شرکت نگاه بانک کارآفرین، شرکت پارمیس، بانک مسکن، شرکت حمل و نقل خلیج فارس، هلدینگ شهرآرا و.... می باشد.</p>
             </div>
         </div>
     </section>
