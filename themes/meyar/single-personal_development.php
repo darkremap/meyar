3086b5ad3e3497d84c9648a3ca1944f720492ae7
Mohammad <remap0095@yahoo.com>
2025-11-13 16:36:14 +0330

    create the single Personal Development posts

---
+++ themes/meyar/single-personal_development.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying single Personal Development posts
+ */
+get_header(); ?>
+
+<div class="container">
+    <h1>personal ina</h1>
+</div>
+
+<?php get_footer(); ?>
