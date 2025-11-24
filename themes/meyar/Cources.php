bdbb07923cccd343af9603b274c624f9f706c52f
Mohammad <remap0095@yahoo.com>
2025-11-24 15:19:03 +0330

    work on courses page

--- themes/meyar/Cources.php
+++ themes/meyar/Cources.php
@@ -14,7 +14,18 @@ get_header(); ?>
         </div>
     </div>
     <div class="container c-filter">
-
+        <div class="row">
+            <div class="c-filter-search">
+                <div class="c-filter-input-group ">
+                    <h2 class="Dana-Bold">جستجو</h2>
+                    <input type="text" class="c-filter-form-control"/>
+                    <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="19.799" cy="13.435" r="8" transform="rotate(45 19.799 13.435)" stroke="#999999" stroke-width="3"/>
+                        <rect x="13.4347" y="17.6777" width="4" height="11" rx="2" transform="rotate(45 13.4347 17.6777)" fill="#999999"/>
+                    </svg>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="container c-cards">
         <div class="row">
@@ -23,10 +34,13 @@ get_header(); ?>
                     <div class="row">
                         <div class="col-md-9 c-card-info">
                             <div class="c-card-info-title">
-                                <h1 class="Dana-Black">عنوان</h1>
+                                <h1 class="Dana-Black">عنوان <?php echo $i  ?></h1>
                                 <div class="c-card-actions">
                                     <a class="Dana-Bold meyar-orange-btn" href="#">مشاهده دوره</a>
                                 </div>
+                            </div>
+                            <div class="c-card-info-thicher">
+                                <h2 class="Dana-Bold">دکتر مهدی مسلمی فر</h2>
                             </div>
                             <div class="c-card-info-desc">
                                 <p class="Dana-Regular">آنچه باید بدان توجه کنیم این است که تفکر سیستمی بیش از آنکه مجموعه‌ای از ابزارها و روش‌ها باشد، یک فلسفه زیربنایی است. بسیاری از ما به امید حل مسائل اغلب تجاری‌مان دست به دامن ابزارهای رایج در تفکر سیستمی می‌شویم. </p>
@@ -38,6 +52,12 @@ get_header(); ?>
                     </div>
                 </div>
             <?php endfor; ?> 
+        </div>
+    </div>
+    <div class="container c-otherCourses">
+         <div class="singleService-pointTitle">
+            <span></span>
+            <h1 class="singleService-text-title Dana-Black">سایر دوره های آموزشی</h1>
         </div>
     </div>
 </section>
