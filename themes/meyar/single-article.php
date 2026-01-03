87851d0734bc73ed7418190e14e5438c1d8bc53d
Mohammad <remap0095@yahoo.com>
2026-01-03 15:39:30 +0330

    create the single article page

--- themes/meyar/single-article.php
+++ themes/meyar/single-article.php
@@ -1,31 +1,44 @@
 <?php get_header(); ?>
 
 <main id="main">
-    <?php
-    if ( have_posts() ) :
-        while ( have_posts() ) : the_post();
-    ?>
-
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <h1><?php the_title(); ?></h1>
-
-            <?php if ( has_post_thumbnail() ) : ?>
-                <div class="article-thumbnail">
-                    <?php the_post_thumbnail('large'); ?>
+    <section class="basePages">
+        <div class="container basePages-header">
+            <div class="row contactUs-header">
+                <div class="col-md-6 contactUs-title">
+                    <h1 class="Dana-Black">دانشنامه</h1>
+                    <h2 class="Dana-Regular">Articles</h2>
                 </div>
-            <?php endif; ?>
-
-            <div class="article-content">
-                <?php the_content(); ?>
+                <div class="col-md-6 contactUs-image">
+                </div>
             </div>
-
-            <?php comments_template(); ?>
-        </article>
-
-    <?php
-        endwhile;
-    endif;
-    ?>
+        <?php
+        if ( have_posts() ) :
+            while ( have_posts() ) : the_post();
+        ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <div class="singleArticle">
+                    <div class="singleArticle-image">
+                        <?php if ( get_field('articleheroimage') ) : ?>
+                            <img src="<?php echo get_field('articleheroimage')['url']; ?>" alt="">
+                        <?php endif; ?> 
+                    </div>     
+                    <h1 class="Dana-Black"><?php the_title(); ?></h1>
+                    <?php if ( has_post_thumbnail() ) : ?>
+                        <div class="article-thumbnail">
+                            <?php the_post_thumbnail('large'); ?>
+                        </div>
+                    <?php endif; ?>
+                    <!-- baraye content style kamel bede -->
+                    <div class="Dana-Medium article-content">
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+            </article>
+        <?php
+            endwhile;
+        endif;
+        ?>
+    </section>
 </main>
 
 <?php get_footer(); ?>
