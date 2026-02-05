9769004af71fc5127f8b14b10e478df1e5754e11
Mohammad <remap0095@yahoo.com>
2026-02-05 17:11:22 +0330

    add collaboration page

---
+++ themes/meyar/collaboration.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Template Name: collaboration
+ */
+get_header(); ?>
+
+
+<section class="collaboration">
+    <div class="container">
+        <div class="collaboration-bace-image">
+            <img  src="<?php echo get_template_directory_uri(); ?>/assets/images/collaboration.png" alt="">
+        </div>
+        <div class="collaboration-cards">
+            <div class="row">
+                <div class="col-6 collaboration-item-right">
+                    <div class="line"></div>
+                    <span class="Dana-Bold">01</span>
+                    <div class="collaboration-card">
+                        <h1 class="Dana-ExtraBold">همکاری آموزشی</h1>
+                        <p class="Dana-DemiBold">اساتیـد و مدرسیـن محترم می‌توانند با تکمیـل ایـن فرم، دوره‌هـای پیشنـهادی خود را ارائه نمایند.</p>
+                    </div>
+                </div>
+                <div class="col-6 collaboration-item-left">
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 collaboration-item-right">
+                </div>
+                <div class="col-6 collaboration-item-left">
+                    <div class="line"></div>
+                    <span class="Dana-Bold">02</span>
+                    <div class="collaboration-card">
+                        <h1 class="Dana-ExtraBold">همکاری سازمانی</h1>
+                        <p class="Dana-DemiBold">سـازمـان‌هـا و شـرکـت‌ها مـی‌تواننـد بـا تکمیل این فرم، خدمات مورد نیاز خود را از معیار درخواست نمایند.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 collaboration-item-right">
+                    <div class="line"></div>
+                    <span class="Dana-Bold">03</span>
+                    <div class="collaboration-card">
+                        <h1 class="Dana-ExtraBold">هـم‌افـزایـی و مشـارکـت</h1>
+                        <p class="Dana-DemiBold">سایر برندها با تکمیل این فرم، می‌توانند محصولات و یـا خدمـات خود را با معیار به اشتراک بگذارند.</p>
+                    </div>
+                </div>
+                <div class="col-6 collaboration-item-left">
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 collaboration-item-right">
+                </div>
+                <div class="col-6 collaboration-item-left">
+                    <div class="line"></div>
+                    <span class="Dana-Bold">04</span>
+                    <div class="collaboration-card">
+                        <h1 class="Dana-ExtraBold">هـم‌افـزایـی و مشـارکـت</h1>
+                        <p class="Dana-DemiBold">اشخاص حقیقی که درخواست همکاری بـا معیـار را دارنـد بـا تکمیـل ایـن فـرم، رزومه خود را ارسال نمایند.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="collaboration-cloud-image">
+        <img  src="<?php echo get_template_directory_uri(); ?>/assets/images/cloud.png" alt="">
+    </div>
+</section>
+
+<?php get_footer(); ?>
