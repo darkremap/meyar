7411bafd1f62282e1dda520802432fc0b01837e3
Mohammad <remap0095@yahoo.com>
2026-02-05 10:43:56 +0330

    add mobile style for contact us

--- themes/meyar/contact-us.php
+++ themes/meyar/contact-us.php
@@ -16,14 +16,14 @@ get_header(); ?>
                 <img  src="<?php echo get_template_directory_uri(); ?>/assets/images/contactUs.png" alt="">
             </div>
         </div>
-        <div class="row">
+        <div class="row contactUs-addressinfo">
             <div class="col-md-6 contactUs-address">
-                <h4 class="Dana-Black mb-5"><span>نشانی :</span> یــوسف آبــاد، بـــالاتـــر از پمـپ بنــزیـــن، (روبـه روی بـانک کارآفرین/جنـب نوین چـرم) ساختـمان پزشـکان ۵۰۱، طبقه ۴، واحد ۱۷</h4>
+                <h4 class="Dana-Black mb-md-5"><span>نشانی :</span> یــوسف آبــاد، بـــالاتـــر از پمـپ بنــزیـــن، (روبـه روی بـانک کارآفرین/جنـب نوین چـرم) ساختـمان پزشـکان ۵۰۱، طبقه ۴، واحد ۱۷</h4>
                 <h4 class="Dana-Black"><span>شماره تماس :</span> 4455667788-021</h4>
                 <h4 class="Dana-Black"><span>ایمیل :</span> meyar@gmail.com</h4>
             </div>
             <div class="col-md-6 contact-map contactUs-location">
-
+                <img  src="<?php echo get_template_directory_uri(); ?>/assets/images/location.png" alt="">
             </div>
         </div>
     </div>
